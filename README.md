5e7352ba87a07a7a43ccc363fe17b63230873fa0
Dave Bunten <dave.bunten@cuanschutz.edu>
2024-08-14 13:08:48 -0600

    Add acknowledgements statement (#94)
    * add acknowledgements statement
    
    * add citation.cff entry
    
    * adjust location of acknowledgement
    
    * fix for build copy

--- README.md
+++ README.md
@@ -10,3 +10,7 @@ Please see the following links to access published content:
 ## CONTRIBUTING
 
 Please see our [`CONTRIBUTING.md`](CONTRIBUTING.md) document for more information on how to contribute to this project.
+
+## Acknowledgements
+
+This work was supported by the Better Scientific Software Fellowship Program, a collaborative effort of the U.S. Department of Energy (DOE), Office of Advanced Scientific Research via ANL under Contract DE-AC02-06CH11357 and the National Nuclear Security Administration Advanced Simulation and Computing Program via LLNL under Contract DE-AC52-07NA27344; and by the National Science Foundation (NSF) via SHI under Grant No. 2327079.
--- src/almanack/__init__.py
+++ src/almanack/__init__.py
@@ -1,4 +1,15 @@
-# __init__.py for software gardening almanack python package
+"""
+__init__.py for software gardening almanack python package
+
+Acknowledgements:
+
+This work was supported by the Better Scientific Software Fellowship Program,
+a collaborative effort of the U.S. Department of Energy (DOE), Office of
+Advanced Scientific Research via ANL under Contract DE-AC02-06CH11357 and the
+National Nuclear Security Administration Advanced Simulation and Computing
+Program via LLNL under Contract DE-AC52-07NA27344; and by the National Science
+Foundation (NSF) via SHI under Grant No. 2327079.
+"""
 
 from .book import read
 from .processing.calculate_entropy import (
